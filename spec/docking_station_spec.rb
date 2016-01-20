a9b5738426e75889e3a65ec8826b89aa75afc1fa
frazerWatson <frazerw@tutanota.com>
2016-01-20 12:40:16 +0000

    third commit

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+require 'boris_bikes'
+
+describe DockingStation do
+  it { expect(subject).to respond_to :release_bike}
+
+  it 'releases working bikes' do
+  	bike = subject.release_bike
+  	expect(bike).to be_working
+  end
+
+end
